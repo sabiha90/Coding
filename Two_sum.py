67bf965425870db76b5ffd9a3b7e7fd891f9d391
Sabiha Hussain Barlaskar <sabihabarlaskar90@gmail.com>
2017-08-02 18:37:02 -0700

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,16 @@
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+###Solution in python
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j] == target - nums[i]:
+                    return i,j
